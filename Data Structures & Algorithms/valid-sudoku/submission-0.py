80591d2aa564aaad04e5dc2d5751b7d3956d8c8b
Logan Calder <lcalder2022@gmail.com>
2026-08-18 23:47:57 -0700

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+        for r, row in enumerate(board):
+            seen = set()
+            for c, num in enumerate(row):
+                if num == ".":
+                    continue
+                # rows logic
+                if num not in seen:
+                    seen.add(num)
+                else:
+                    return False
+
+                # cols logic
+                if num not in cols[c]:
+                    cols[c].add(num)
+                else:
+                    return False
+
+                # box logic
+                # row index, col index both int div by 3
+                boxcoords = (r // 3, c // 3) 
+                if num not in boxes[str(boxcoords)]:
+                    boxes[str(boxcoords)].add(num)
+                else:
+                    return False
+        return True
+                
+                
